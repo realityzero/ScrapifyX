84eb95103b504aecb48e97347545b07c884a3100
realityzero <nishantsikri@spit.ac.in>
2024-02-23 19:09:25 +0530

    next.config allow external package puppeteer

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        serverComponentsExternalPackages: ['puppeteer-core'],
+    }
+};
 
 export default nextConfig;
