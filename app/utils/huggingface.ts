f724c6fa262726bf6e6720e8161222a55e47cc1f
realityzero <nishantsikri@spit.ac.in>
2024-02-25 01:35:19 +0530

    util: huggingface

---
+++ app/utils/huggingface.ts
@@ -0,0 +1,18 @@
+import { config } from "@/config";
+import { FeatureExtractionOutput, HfInference } from "@huggingface/inference";
+
+export class HuggingFace {
+    private _hf;
+
+    constructor() {
+        this._hf = new HfInference(config.huggingface.apiKey);
+    }
+
+    async generateEmbedding(dataChunk: string): Promise<FeatureExtractionOutput> {
+        const vectorEmbedding = this._hf.featureExtraction({
+            model: config.huggingface.model,
+            inputs: dataChunk.replace(/\n/g, " ")
+        });
+        return vectorEmbedding;
+    }
+}
