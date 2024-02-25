dfe07d7c51507f4c4c2696c2241522b326e04953
realityzero <nishantsikri@spit.ac.in>
2024-02-25 13:35:34 +0530

    [WIP] router

--- app/api/route.ts
+++ app/api/route.ts
@@ -51,29 +51,12 @@ async function getBrowser(): Promise<Browser> {
     // });
     const options = await getOptions(true);
     return puppeteer.launch(options);
+
 }
-import { HfInference } from '@huggingface/inference';
-import { chunk } from 'llm-chunk';
-// const getDownloads = async () => {
-//     console.log('Hey!');
-//     const res = await fetch('https://www.npmjs.com/package/puppeteer');
-//     const html = await res.text();
-
-// }
-// export async function GET() {
-//     console.log('Hey!');
-//     const res = await fetch('https://www.npmjs.com/package/puppeteer', {
-//     });
-//     const html = await res.text();
-//     const data = await res.json()
-   
-//     return Response.json({ data })
-//   }
-  
-
-// export default getDownloads;
 import { headers } from 'next/headers'
 import { Browser, Page } from 'puppeteer-core';
+import { PineconeDb } from '../utils/pinecone';
+import { config } from '@/config';
  
 // export async function GET(request: Request) {
 //   const headersList = headers()
@@ -86,14 +69,6 @@ import { Browser, Page } from 'puppeteer-core';
 // }
 
 export async function POST() {
-    // const res = await fetch('https://data.mongodb-api.com/...', {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //     'API-Key': process.env.DATA_API_KEY!,
-    //   },
-    //   body: JSON.stringify({ time: new Date().toISOString() }),
-    // })
     // const res = await fetch('https://www.npmjs.com/package/puppeteer');
     // const html = await res.text();
     // console.log(html);
@@ -107,7 +82,7 @@ export async function POST() {
     // return Response.json(data)
     try {
         // const file = await getScreenshot('https://www.npmjs.com/package/puppeteer');
-        const file = await getScreenshot('https://arxiv.org/abs/2310.05421');
+        const file = await getScreenshot('https://www.oziva.in/products/oziva-protein-herbs-men-whey-protein-with-ayurvedic-herbs-multivitamins-soy-free-gluten-free-no-preservatives?variant=20052525711419');
         return new Response(file, {
             status: 200,
             headers: {
@@ -115,19 +90,8 @@ export async function POST() {
                 'Cache-Control': 'public, immutable, no-transform, s-maxage=604800, max-age=604800',
             },
         })
-        // res.setHeader('Content-Type', 'image/png');
-        // res.setHeader(
-        //   'Cache-Control',
-        //   'public, immutable, no-transform, s-maxage=604800, max-age=604800'
-        // );
-        // res.status(200).end(file);
       } catch (error) {
         console.error(error);
-        // res
-        //   .status(500)
-        //   .send(
-        //     'The server encountered an error. You may have inputted an invalid query.'
-        //   );
         return new Response('The server encountered an error. You may have inputted an invalid query.', {
         status: 500,
     })
@@ -155,48 +119,45 @@ export async function POST() {
   export async function getScreenshot(url: string, ratio = 1) {
     const page = await getPage();
     await page.goto(url, {
-      waitUntil: 'domcontentloaded',
+      waitUntil: 'networkidle0',
     });
     await page.setViewport({
       width: 1000,
       height: 600,
       devicePixelRatio: ratio,
     });
+    const pageTitle = await page.title();
+    console.log('pageTitle', pageTitle);
+    
     const extractedText = await page.$eval('*', (el) => el.innerText);
     console.log('extractedText', extractedText);
-    const chunks = chunk(extractedText, {
-      minLength: 5,          // number of minimum characters into chunk
-      maxLength: 2048,       // number of maximum characters into chunk
-      splitter: "paragraph", // paragraph | sentence
-      overlap: 0,            // number of overlap chracters
-      delimiters: ""         // regex for base split method
-    });
-    // console.log('chunks', chunks);
-    console.log('printing chunks', chunks.length);
 
-
-    const hf = new HfInference('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')
-    const vectorArray = [];
-    
-    for (const chunk of chunks) {
-      console.log('-------------------------------------------------------');
-      console.log(chunk);
-      // const vectorEmbedding = await hf.featureExtraction({
-      //   model: 'sentence-transformers/all-MiniLM-L6-v2',
-      //   inputs: chunk
+    try {
+      // await createPineconeIndex(client, indexName, vectorDimensions);
+      // await updatePineconesss(client, indexName, {
+      //   content: extractedText,
+      //   title: pageTitle,
+      //   url: 'https://arxiv.org/abs/2310.05421',
       // });
-      // vectorArray.push(vectorEmbedding);
+      // await new PineconeDb().createIndex(config.pinecode.indexName, config.pinecode.vectorDimensions);
+      await new PineconeDb().queryVectorStore(config.pinecode.indexName, 'How does protein work?');
+      // await new PineconeDb().upsertDb(config.pinecode.indexName, {
+      //     content: extractedText,
+      //     title: pageTitle,
+      //     url: 'https://www.oziva.in/products/oziva-protein-herbs-men-whey-protein-with-ayurvedic-herbs-multivitamins-soy-free-gluten-free-no-preservatives?variant=20052525711419',
+      // });
+      // await queryPineconeVectorStoreAndQueryLLM(client, indexName, 'Can we automate customer service using LangChain?');
+    } catch (err) {
+      console.log('error: ', err)
     }
-    console.log('printing vector embeddings', vectorArray);
+
     const file = await page.screenshot();
     // const screenshot = await page.screenshot({
     //     encoding: 'base64',
     // });
 
     await page.close();
-    // res.json({ blob: `data:image/jpeg;base64,${screenshot}` });
     return file;
-    // return `data:image/jpeg;base64,${screenshot}`;
   }
   
 //   module.exports = async (req, res) => {
