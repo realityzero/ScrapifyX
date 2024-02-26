edefae029e587e15639a888de909e420410faf7f
realityzero <nishantsikri@spit.ac.in>
2024-02-26 17:02:00 +0530

    allow next to fetch remote images

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,17 @@
 const nextConfig = {
     experimental: {
         serverComponentsExternalPackages: ['puppeteer-core'],
-    }
+    },
+    images: {
+        remotePatterns: [
+            {
+            protocol: "https",
+            hostname: "*.githubusercontent.com",
+            port: "",
+            pathname: "**",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
