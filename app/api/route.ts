c94bc036eaf34d64f832d1a6b8f9b134ea9b1042
realityzero <nishantsikri@spit.ac.in>
2024-02-27 01:01:30 +0530

    cleanup, attempt performance improvement

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,46 +1,6 @@
-import { Browser, Page } from 'puppeteer-core';
-import { PineconeDb } from '../utils/pinecone';
-import { config } from "../../config";
-
-const chromium = require('@sparticuz/chromium-min');
-const puppeteer = require('puppeteer-core');
 import { z } from "zod";
+import { Puppeteer } from "../utils/puppeteer";
 
-const exePath =
-  process.platform === "win32"
-    ? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-    : process.platform === "linux"
-    ? "/usr/bin/google-chrome"
-    : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
-
-async function getOptions(isDev: boolean) {
-    let options;
-    console.log('exePath:', exePath);
-    if (isDev) {
-        options = {
-        args: [],
-        executablePath: exePath,
-        headless: 'new',
-        };
-    } else {
-        options = {
-            args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
-            defaultViewport: chromium.defaultViewport,
-            executablePath: await chromium.executablePath(
-                `https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar`
-            ),
-            headless: chromium.headless,
-            ignoreHTTPSErrors: true,
-        };
-    }
-    return options;
-}
-
-async function getBrowser(): Promise<Browser> {
-    const options = await getOptions(config.isDevMode);
-    return puppeteer.launch(options);
-
-}
 
 export async function POST(request: Request) {
       const schema = z.object({
@@ -60,7 +20,7 @@ export async function POST(request: Request) {
 
     const { url } = zodCheck.data;
     try {
-        await initiateScraping(url);
+        await new Puppeteer().initiateScraping(url);
         return Response.json('Success', {
           status: 200,
         })
@@ -71,42 +31,3 @@ export async function POST(request: Request) {
     })
       }
   }
-
-  async function getPage(): Promise<Page> {
-    const browser = await getBrowser();
-    const page = await browser.newPage();
-    return page;
-  }
-  
-  async function initiateScraping(url: string, ratio = 1) {
-    const page = await getPage();
-    await page.goto(url, {
-      timeout: 0,
-      waitUntil: 'networkidle0',
-    });
-    await page.setViewport({
-      width: 1000,
-      height: 600,
-      deviceScaleFactor: ratio
-    });
-    const pageTitle = await page.title();
-    console.log('pageTitle', pageTitle);
-    
-    // @ts-ignore
-    const extractedText = await page.$eval('*', (el) => el.innerText);
-    // close page for better memory management
-    await page.close();
-
-    try {
-      await new PineconeDb().createIndex(config.pinecode.indexName, config.pinecode.vectorDimensions);
-      await new PineconeDb().upsertDb(config.pinecode.indexName, {
-          content: extractedText,
-          title: pageTitle,
-          url,
-      });
-    } catch (err) {
-      console.log('error: ', err);
-      throw err;
-    }
-  }
-  
---
+++ app/utils/puppeteer.ts
@@ -0,0 +1,88 @@
+import { Browser, Page } from 'puppeteer-core';
+import { PineconeDb } from '../utils/pinecone';
+import { config } from "../../config";
+
+const chromium = require('@sparticuz/chromium-min');
+const puppeteer = require('puppeteer-core');
+export class Puppeteer {
+    static exePath =
+    process.platform === "win32"
+        ? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+        : process.platform === "linux"
+        ? "/usr/bin/google-chrome"
+        : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
+
+    static options: { args: never[] | any[]; executablePath: any; headless: any; defaultViewport?: any; ignoreHTTPSErrors?: boolean; };
+
+
+    async getOptions(isDev: boolean) {
+        // let options;
+        console.log('exePath:', Puppeteer.exePath);
+        if (Puppeteer.options && Object.keys(Puppeteer.options).length !== 0) {
+            console.log(JSON.stringify(Puppeteer.options));
+            return Puppeteer.options;
+        }
+        if (isDev) {
+            Puppeteer.options = {
+            args: [],
+            executablePath: Puppeteer.exePath,
+            headless: 'new',
+            };
+        } else {
+            Puppeteer.options = {
+                args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
+                defaultViewport: chromium.defaultViewport,
+                executablePath: await chromium.executablePath(
+                    `https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar`
+                ),
+                headless: chromium.headless,
+                ignoreHTTPSErrors: true,
+            };
+        }
+        return Puppeteer.options;
+    }
+
+    async getBrowser(): Promise<Browser> {
+        const options = await this.getOptions(config.isDevMode);
+        return puppeteer.launch(options);
+
+    }
+
+    async getPage(): Promise<Page> {
+        const browser = await this.getBrowser();
+        const page = await browser.newPage();
+        return page;
+    }
+
+    async initiateScraping(url: string, ratio = 1) {
+        const page = await this.getPage();
+        await page.goto(url, {
+            timeout: 0,
+            waitUntil: 'networkidle0',
+        });
+        await page.setViewport({
+            width: 1000,
+            height: 600,
+            deviceScaleFactor: ratio
+        });
+        const pageTitle = await page.title();
+        console.log('pageTitle', pageTitle);
+
+        // @ts-ignore
+        const extractedText = await page.$eval('*', (el) => el.innerText);
+        // close page for better memory management
+        await page.close();
+
+        try {
+            await new PineconeDb().createIndex(config.pinecode.indexName, config.pinecode.vectorDimensions);
+            await new PineconeDb().upsertDb(config.pinecode.indexName, {
+                content: extractedText,
+                title: pageTitle,
+                url,
+            });
+        } catch (err) {
+            console.log('error: ', err);
+            throw err;
+        }
+    }
+}
